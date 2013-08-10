a1576e61788a4b2b66877d94899ba67b38d512f8
Luan Santos <cfcluan@gmail.com>
2013-08-10 10:03:28 -0700

    Remove weird comments from App

--- tibia_api.rb
+++ tibia_api.rb
@@ -9,13 +9,6 @@ module TibiaAPI
   class App < Sinatra::Base
     get '/api/worlds.json' do
       content_type :json
-
-      # content = open('http://tibia.com/community/?subtopic=worlds').read
-      # doc = Nokogiri::HTML.parse(content)
-
-      # require 'awesome_print'
-      # ap doc.at_css('#worlds table tr td').text
-
       worlds = TibiaAPI::World.all.map(&:to_json)
       { worlds: worlds }.to_json
     end
